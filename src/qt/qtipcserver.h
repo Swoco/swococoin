6fc674c3f69dc63ec89030fd49a95221c9e4a68c
Swoco <infoswoco@gmail.com>
2019-01-07 12:30:08 +0530

    Add existing file

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define SWOCO-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "SWOCOURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
